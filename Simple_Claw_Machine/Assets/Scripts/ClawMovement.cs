3770c335354a64febfad79a44246fd3669c0ff3d
Galuh Kirana Miftahuljannah <galuhkmj@gmail.com>
2023-11-08 10:07:03 +0700

    limit motor movement

--- Simple_Claw_Machine/Assets/Scripts/ClawMovement.cs
+++ Simple_Claw_Machine/Assets/Scripts/ClawMovement.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ClawMovement : MonoBehaviour
@@ -7,32 +5,59 @@ public class ClawMovement : MonoBehaviour
     public GameObject LeftRight;
     public GameObject BackFront;
 
-    private const float SPEED = 5f;
+    private const float SPEED = 2f;
+
+    private const float LEFT_LIMIT = -1.6f;
+    private const float RIGHT_LIMIT = 0.6f;
+    private const float BACK_LIMIT = -1.6f;
+    private const float FRONT_LIMIT = -4.1f;
 
     private void Update()
     {
         if (Input.GetKey(KeyCode.RightArrow))
         {
             Vector3 newPos = new Vector3(SPEED * Time.deltaTime, 0, 0);
-            LeftRight.transform.position += newPos;
+
+            if (LeftRight.transform.position.x + newPos.x < RIGHT_LIMIT)
+            {
+                LeftRight.transform.position += newPos;
+            }
+
         }
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             Vector3 newPos = new Vector3(-(SPEED * Time.deltaTime), 0, 0);
-            LeftRight.transform.position += newPos;
+
+            if (LeftRight.transform.position.x + newPos.x > LEFT_LIMIT)
+            {
+                LeftRight.transform.position += newPos;
+            }
         }
 
-        if(Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.UpArrow))
         {
             Vector3 newPos = new Vector3(0, 0, SPEED * Time.deltaTime);
-            BackFront.transform.position += newPos;
+
+            if(BackFront.transform.position.z + newPos.z < BACK_LIMIT)
+            {
+
+                BackFront.transform.position += newPos;
+            }
+
         }
 
         if (Input.GetKey(KeyCode.DownArrow))
         {
             Vector3 newPos = new Vector3(0, 0, -(SPEED * Time.deltaTime));
-            BackFront.transform.position += newPos;
+
+            Debug.Log(BackFront.transform.position.z);
+
+            if (BackFront.transform.position.z + newPos.z > FRONT_LIMIT)
+            {
+                BackFront.transform.position += newPos;
+            }
+
         }
 
 
