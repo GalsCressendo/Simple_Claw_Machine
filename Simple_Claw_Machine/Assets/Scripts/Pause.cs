1243713c2f8ae6b2f361ec5e52366ea0e3fbeb65
Galuh Kirana Miftahuljannah <galuhkmj@gmail.com>
2023-11-10 13:13:57 +0700

    enable pause button

--- Simple_Claw_Machine/Assets/Scripts/Pause.cs
+++ Simple_Claw_Machine/Assets/Scripts/Pause.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngineInternal;
 
 public class Pause : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject pausePanel;
+    [SerializeField] private Button pauseButton;
+
+    private void OnEnable()
     {
-        
+       pauseButton.onClick.AddListener(EnablePausePanel);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void EnablePausePanel()
     {
-        
+        if(!pausePanel.activeInHierarchy)
+        {
+            pausePanel.SetActive(true);
+        }
+
+        pauseButton.onClick.RemoveListener(EnablePausePanel);
+        pauseButton.transform.parent.gameObject.SetActive(false);
     }
 }
