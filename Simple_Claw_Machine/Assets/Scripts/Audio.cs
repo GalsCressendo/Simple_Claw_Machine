340a5d5bac308af0b69da95e2f0035d6eb2042ac
Galuh Kirana Miftahuljannah <galuhkmj@gmail.com>
2023-11-10 15:50:54 +0700

    implement audio manager

---
+++ Simple_Claw_Machine/Assets/Scripts/Audio.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+    [Serializable]
+    public class AudioAttribute
+    {
+        public string audioName;
+        public AudioClip clip;
+        public bool isLoop;
+        public bool playOnAwake;
+        public AudioSource source;
+    }
+
+    public const string BGM_AUDIO = "BGM";
+    public const string CLAW1_AUDIO = "Claw1";
+    public const string CLAW2_AUDIO = "Claw2";
+    public const string START_AUDIO = "Start";
+
+    public AudioAttribute[] audioAttribute;
+
+    private void Awake()
+    {
+        foreach(var attribute in audioAttribute)
+        {
+            AudioSource au = gameObject.AddComponent<AudioSource>();
+            au.name = attribute.audioName;
+            au.clip = attribute.clip;
+            au.loop = attribute.isLoop;
+            au.playOnAwake = attribute.playOnAwake;
+            attribute.source = au;
+        }
+    }
+
+    private void Start()
+    {
+        Play(BGM_AUDIO);
+        DecreaseVolume(BGM_AUDIO, 0.5f);
+    }
+
+    public void Play(string audioName)
+    {
+        AudioAttribute au = Array.Find(audioAttribute, s => s.audioName == audioName);
+        au.source.Play();
+    }
+
+    public void Stop(string audioName)
+    {
+        AudioAttribute au = Array.Find(audioAttribute, s => s.audioName == audioName);
+        au.source.Stop();
+    }
+
+    public void RaiseVolume(string audioName, float valueIncrease)
+    {
+        AudioAttribute au = Array.Find(audioAttribute, s => s.audioName == audioName);
+        au.source.volume += valueIncrease;
+    }
+
+    public void DecreaseVolume(string audioName, float valueDecrease)
+    {
+        AudioAttribute au = Array.Find(audioAttribute, s => s.audioName == audioName);
+        au.source.volume -= valueDecrease;
+    }
+
+}
